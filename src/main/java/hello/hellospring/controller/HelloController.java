7952fce6b1de4b9e724442dff1dfa5ddec90115d
DavidJeonKr <dudgh7410@naver.com>
2022-05-07 20:56:30 +0900

    테스트 케이스

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "good to see you!!");
 
         return "hello";
-    };
+    }
+
+    ;
 
 
     //api @ResponseBody 문자 반환
